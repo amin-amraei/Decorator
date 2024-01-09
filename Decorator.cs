eb1df382c43527ff0e09bcdb4aeb0633d9f99d5e
a.amraei <amin.amraei@gmail.com>
2024-01-09 08:44:38 +0330

    Add project files.

---
+++ Decorator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+        public Decorator(Component component)
+        {
+            _component = component;
+        }
+        public override string Operation()
+        {
+            if (_component != null)
+            {
+                return _component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
